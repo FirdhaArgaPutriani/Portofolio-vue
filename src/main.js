d98f630284408865d94a4589a3cef35be57b1394
Firdha Arga Putriani <kuroba11kaito@gmail.com>
2025-08-30 06:14:44 +0700

    HeroSection

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { Icon } from '@iconify/vue'
+import Aos from 'aos'
+import 'aos/dist/aos.css' 
 
 createApp(App).component('Icon',Icon).mount('#app')
+
+Aos.init({ duration:1000 });
