9bc09ba9e40b8980475b8fe18607aee7315fb6f8
Firdha Arga Putriani <kuroba11kaito@gmail.com>
2025-08-30 20:44:58 +0700

    CounterSection

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import './style.css'
 import App from './App.vue'
 import { Icon } from '@iconify/vue'
 import Aos from 'aos'
-import 'aos/dist/aos.css' 
+import 'aos/dist/aos.css'
+import Countup from 'vue-countup-v3'
 
-createApp(App).component('Icon',Icon).mount('#app')
+createApp(App).component('Icon',Icon).component('Countup',Countup).mount('#app')
 
 Aos.init({ duration:1000 });
