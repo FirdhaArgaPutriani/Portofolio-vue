a64f186614e030a893c8657c892e20d2ba14e6ea
Firdha Arga Putriani <kuroba11kaito@gmail.com>
2025-08-31 12:55:57 +0700

    ServiceSection

--- src/components/layout/Services.vue
+++ src/components/layout/Services.vue
@@ -1,9 +1,24 @@
 <template>
     <section id="services" class="relative text-white mt-32">
-        <SectionHead />
-        <div class="mt-20 py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+        <SectionHead title="My Services" />
+        <div
+            class="mt-20 py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
             <article class="px-[12px] mb-[24px]" v-for="element in services" :key="element.id">
-                <div class="mt-[]"></div>
+                <div class="mt-[30px] relative flex flex-col justify-center text-center rounded-[30px] border border-secondary 
+                dark:bg-[#ffffff29] bg-primary" data-aos="fade-up" :data-aos-delay="element.id * 100">
+                    <div class="mt-[-30px] flex justify-center relative">
+                        <div class="mt-[-7px] p-[20px] relative bg-primary rounded-[50%]">
+                            <span class="h-[60px] w-[60px] m-auto border-[1px] border-solid border-secondary absolute top-[8px] left-[8px]
+                            rounded-[50%] inline-block z-[5]"></span>
+                            <img :src="element.icon" alt="services" class="h-[35px] w-[35px] relative z-[1]" />
+                        </div>
+                    </div>
+                    <div class="p-[30px]">
+                        <h3 class="uppercase font-semibold text-secondary text-base">{{ element.name }}</h3>
+                        <p class="text-[14px] leading-[20px] text-white tracking-[0.03rem]">{{ element.description }}
+                        </p>
+                    </div>
+                </div>
             </article>
         </div>
     </section>
@@ -12,40 +27,40 @@
 import SectionHead from '../UI/SectionHead.vue';
 import { ref } from 'vue';
 
-const services = ref ([
+const services = ref([
     {
         id: 1,
-        icon: 'https://img.icons8.com/ios-filled/100/deadla/google-code.png',
+        icon: 'https://img.icons8.com/ios-filled/100/dead1a/google-code.png',
         name: 'Web Development',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius totam ducimus recusandae? Aut fugit similique, id facilis possimus at impedit.'
     },
     {
         id: 2,
-        icon: 'https://img.icons8.com/external-vectorslab-glyph-vectorslab/100/deadla/external-Technical-Writing-mobile-app-development-vectorslab-gryph-vectorslab.png',
+        icon: 'https://img.icons8.com/external-vectorslab-glyph-vectorslab/100/dead1a/external-Technical-Writing-mobile-app-development-vectorslab-glyph-vectorslab.png',
         name: 'Technical Writing',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius totam ducimus recusandae? Aut fugit similique, id facilis possimus at impedit.'
     },
     {
         id: 3,
-        icon: 'https://img.icons8.com/external-solid-design-circle/100/deadla/external-Mobile-digital-marketing-solid-design-circle.png',
+        icon: 'https://img.icons8.com/ios-filled/100/dead1a/email-open.png',
         name: 'Technical Writing',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius totam ducimus recusandae? Aut fugit similique, id facilis possimus at impedit.'
     },
     {
         id: 4,
-        icon: 'https://img.icons8.com/ios-filled/100/deadla/email-open.png',
+        icon: 'https://img.icons8.com/ios-filled/100/dead1a/email-open.png',
         name: 'Technical Writing',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius totam ducimus recusandae? Aut fugit similique, id facilis possimus at impedit.'
     },
     {
         id: 5,
-        icon: 'https://img.icons8.com/ios-filled/100/deadla/windows10-personalization.png',
+        icon: 'https://img.icons8.com/ios-filled/100/dead1a/windows10-personalization.png',
         name: 'Technical Writing',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius totam ducimus recusandae? Aut fugit similique, id facilis possimus at impedit.'
     },
     {
         id: 6,
-        icon: 'https://img.icons8.com/ios-filled/100/deadla/web-design.png',
+        icon: 'https://img.icons8.com/ios-filled/100/dead1a/web-design.png',
         name: 'Technical Writing',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius totam ducimus recusandae? Aut fugit similique, id facilis possimus at impedit.'
     },
