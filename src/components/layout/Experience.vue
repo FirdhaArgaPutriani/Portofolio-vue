30bcb1c0d03820d417a7c67dcf05fbd4ab302e8d
Firdha Arga Putriani <kuroba11kaito@gmail.com>
2025-09-02 20:49:17 +0700

    Whymesection

--- src/components/layout/Experience.vue
+++ src/components/layout/Experience.vue
@@ -1,22 +1,22 @@
 <template>
     <section class="p-16">
         <header class="text-center">
-            <h3 class="text-4xl font-bold text-primary dark:text-white">Education</h3>
+            <h3 class="text-4xl font-bold text-primary dark:text-white">Experience</h3>
         </header>
         <ul class="mt-10 space-y-8 y-8">
             <li class="w-full rounded-xl shadow-lg border bg-gradient-to-t to-[#ACAC39] from-[#1F1E1C99]"
-                v-for="(item, index) in education" :key="index">
+                v-for="(item, index) in experience" :key="index">
                 <article class="flex flex-col lg:flex-row items-center lg:items-start rounded-[12px] bg-primary
                 mt-[3px] mx-[3px] p-6 md:p-8 lg:p-10">
                     <figure class="w-full lg:w-1/4 flex justify-center lg:justify-start mb-4 lg:mb-0">
-                        <Icon icon="hugeicons:university" class="text-4xl md:text-6xl lg:text-7xl text-secondary"/>
+                        <Icon icon="material-symbols-light:work" class="text-4xl md:text-6xl lg:text-7xl text-secondary"/>
                     </figure>
 
                     <section class="w-full lg:w-3/4 lg:pl-4 text-center lg:text-left">
                         <header>
-                            <h3 class="text-xl md:text-2xl font-semibold uppercase text-secondary">{{ item.school }}</h3>
+                            <h3 class="text-xl md:text-2xl font-semibold uppercase text-secondary">{{ item.title }}</h3>
                         </header>
-                        <p class="text-sm md:text-base text-white">{{ item.degree }}</p>
+                        <p class="text-sm md:text-base text-white">{{ item.company }}</p>
                         <p class="text-sm md:text-base text-white">{{ item.duration }}</p>
                     </section>
                 </article>
@@ -27,16 +27,16 @@
 <script setup>
 import { ref } from 'vue';
 
-const education = ref([
+const experience = ref([
     {
         duration: '03/2008-07/2011',
-        school: 'School 1',
-        degree: 'Software Engineer'
+        title: 'Title 1',
+        company: 'Company 1'
     },
     {
         duration: '03/2008-07/2011',
-        school: 'School 2',
-        degree: 'Software Engineer'
+        title: 'Title 2',
+        company: 'Company 2'
     }
 ])
 </script>
--- src/components/layout/Whyme.vue
+++ src/components/layout/Whyme.vue
@@ -1,12 +1,14 @@
 <template>
     <section id="whyme" class="mt-32">
         <SectionHead title="Why Me?"/>
-        <div class="grid grid-cols-1 lg:grid-cols-1 p-12 overflow-hidden">
+        <div class="grid grid-cols-1 lg:grid-cols-2 p-12 overflow-hidden">
             <Education data-aos="fade-left"/>
+            <Experience data-aos="fade-right"/>
         </div>
     </section>
 </template>
 <script setup>
 import SectionHead from '../UI/SectionHead.vue';
 import Education from './Education.vue';
+import Experience from './Experience.vue';
 </script>
