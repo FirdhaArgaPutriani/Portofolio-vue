63a4af4bd42c796f61eeca921494c722c937b0d1
Firdha Arga Putriani <kuroba11kaito@gmail.com>
2025-09-03 21:32:20 +0700

    ProjectsSection

---
+++ src/components/UI/ProjectCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <article class="max-w-sm overflow-hidden shadow-lg rounded border border-secondary dark:bg-[#FFFFFF29]">
+        <figure>
+            <img :src="image" :alt="title" class="w-full h-48 object-cover">
+        </figure>
+
+        <!-- Content Section -->
+        <section class="px-6 py-6">
+            <header>
+                <h3 class="font-bold text-xl mx-2 text-white">{{ title }}</h3>
+            </header>
+            <p class="text-gray-300 text-sm"{{ description }}></p>
+        </section>
+
+        <!-- Tags Section -->
+        <footer class="px-6 pt-4 pb-2">
+            <ul class="flex flex-wrap gap-2">
+                <li v-for="(tags, index) in tags" :key="index" class="inline-block dark:bg-primary bg-gray-700 text-gray-200 rounded-full px-3 py-1 text-sm font-semibold">
+                    #{{ tags }}
+                </li>
+            </ul>
+        </footer>
+
+        <!-- Actions Section -->
+        <div class="px-6 pb-4 flex justify-between items-center">
+            <a :href="liveLink" v-if="liveLink" class="text-secondary font-semibold hover:underline text-sm">Live Demo</a>
+            <a :href="codeLink" v-if="codeLink" class="text-secondary font-semibold hover:underline text-sm">View Code</a>
+        </div>
+    </article>
+</template>
+<script setup>
+defineProps({
+    title:{
+        type: String,
+        required: true,
+        default: 'Project Title'
+    },
+    description:{
+        type: String,
+        required: true,
+        default: 'Brief desc of the project'
+    },
+    image:{
+        type: String,
+        required: true,
+        default: '/'
+    },
+    tags:{
+        type: Array,
+        required: true,
+        default: ()=> ['Brief desc of the project']
+    },
+    liveLink:{
+        type: String,
+        required: false,
+        default: null
+    },
+    codeLink:{
+        type: String,
+        required: false,
+        default: null
+    },
+})
+</script>
+<style>
+    
+</style>
---
+++ src/components/layout/Projects.vue
@@ -0,0 +1,74 @@
+<template>
+    <section id="projects" class="relative w-11\12 px-16 mx-auto">
+        <SectionHead title="My Projects" />
+        <!-- Custom Nav Button -->
+        <button
+            class="swiper-prev absolute top-2/3 left-4 transform -translate-y-1/2 z-10 text-white dark:bg-secondary bg-primary p-4 rounded-full">
+            <Icon icon="line-md:arrow-left" class="font-bold text-3xl" />
+        </button>
+        <button
+            class="swiper-next absolute top-2/3 right-4 transform -translate-y-1/2 z-10 text-white dark:bg-secondary bg-primary p-4 rounded-full">
+            <Icon icon="line-md:arrow-right" class="font-bold text-3xl" />
+        </button>
+
+        <!-- Swiper Content -->
+        <swiper :effect="'coverflow'" grab-cursor centered-slides :slides-per-view="'auto'"
+            :converflowEffect="{ rotate: 50, stretch: 0, depth: 100, modifer: 1, slideShadow: true }"
+            :Navigation="navOption" pagination :modules="[EffectCoverflow, Navigation]" class="max-w-full mt-32">
+            <swiper-slide class="max-w-[300px]" v-for="(project, index) in projects" :key="index">
+                <ProjectCard :title="project.title" :description="project.description"
+                    :image="'src/assets/images/' + project.image" :tags="project.tags" :liveLinks="project.liveLinks"
+                    :codeLink="project.codeLink" />
+            </swiper-slide>
+        </swiper>
+    </section>
+</template>
+
+<script setup>
+import SectionHead from '../UI/SectionHead.vue';
+import ProjectCard from '../UI/ProjectCard.vue';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { EffectCoverflow, Navigation } from 'swiper/modules';
+import { ref } from 'vue';
+
+// Define nav options
+const navOption = {
+    prevEl: '.swiper-prev',
+    nextEl: '.swiper-next',
+}
+
+const projects = ref([
+    {
+        title: 'Portofolio Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, excepturi.',
+        image: '',
+        tags: ['HTML', 'CSS', 'Javascript'],
+        liveLinks: '/',
+        codeLink: ""
+    },
+    {
+        title: 'Portofolio Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, excepturi.',
+        image: '',
+        tags: ['HTML', 'CSS', 'Javascript'],
+        liveLinks: '/',
+        codeLink: ""
+    },
+])
+</script>
+
+<style>
+.swiper-button-prev,
+.swiper-button-next{
+    cursor: pointer;
+}
+.swiper-slide{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
